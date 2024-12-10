a066bbef7de9c2c0aaa03ccbb56b2f56875ca914
Lydie Orset <lydieorset@gmail.com>
2024-12-10 12:33:24 +0100

    vues workspaces

--- src/Entity/Workspace.php
+++ src/Entity/Workspace.php
@@ -27,6 +27,9 @@ class Workspace
     #[ORM\ManyToMany(targetEntity: Equipment::class, mappedBy: 'workspace')]
     private Collection $equipment;
 
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
     public function __construct()
     {
         $this->equipment = new ArrayCollection();
@@ -84,6 +87,18 @@ class Workspace
         if ($this->equipment->removeElement($equipment)) {
             $equipment->removeWorkspace($this);
         }
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
 
         return $this;
     }
