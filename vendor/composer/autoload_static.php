0070a4332bf8b9e6a0c8e2af28a14a178a8d5be2
Lydie Orset <lydieorset@gmail.com>
2024-12-11 10:51:50 +0100

    suppression full calendar

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -158,7 +158,6 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
         'C' => 
         array (
             'Composer\\Semver\\' => 16,
-            'CalendarBundle\\' => 15,
         ),
         'A' => 
         array (
@@ -585,10 +584,6 @@ class ComposerStaticInit54c05e64f01f49eb136e9af7b3075bdd
         'Composer\\Semver\\' => 
         array (
             0 => __DIR__ . '/..' . '/composer/semver/src',
-        ),
-        'CalendarBundle\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/tattali/calendar-bundle/src',
         ),
         'App\\Tests\\' => 
         array (
